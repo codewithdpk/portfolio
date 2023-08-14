11468c1df4e8a21594b5d1283f37bdf162b36569
Deepak Suthar <codewithdeepak@yahoo.com>
2023-08-15 01:29:52 +0530

    feat: added social links

--- src/theme/ThemeProvider.tsx
+++ src/theme/ThemeProvider.tsx
@@ -1,42 +1,47 @@
-"use client"
+"use client";
 
-import { createContext, Dispatch, ReactNode, SetStateAction, useEffect, useState } from "react";
+import {
+  createContext,
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  useEffect,
+  useState,
+} from "react";
 
-type ThemeMode = 'light' | 'dark'
+type ThemeMode = "light" | "dark";
 
 interface DefaultValuesType {
-    theme: ThemeMode
-    setTheme: Dispatch<SetStateAction<"light" | "dark">>
+  theme: ThemeMode;
+  setTheme: Dispatch<SetStateAction<"light" | "dark">>;
 }
 const defaultValues: DefaultValuesType = {
-    theme: 'dark',
-    setTheme: () => {
-        return
-    }
-}
-const ThemeContext = createContext(defaultValues)
+  theme: "dark",
+  setTheme: () => {
+    return;
+  },
+};
+const ThemeContext = createContext(defaultValues);
 
 export default function ThemeProvider({ children }: { children: ReactNode }) {
+  const [theme, setTheme] = useState<ThemeMode>("dark");
 
-    const [theme, setTheme] = useState<ThemeMode>('light')
+  useEffect(() => {
+    const existingTheme = localStorage.getItem("theme");
+    if (existingTheme) {
+      setTheme(existingTheme as ThemeMode);
+      document.documentElement.classList.add(existingTheme);
+    } else {
+      localStorage.setItem("theme", theme);
+      document.documentElement.classList.add(theme);
+    }
+  }, []);
 
-    useEffect(() => {
-        const existingTheme = localStorage.getItem("theme")
-        if (existingTheme) {
-            setTheme(existingTheme as ThemeMode)
-            document.documentElement.classList.add(existingTheme)
-        } else {
-            localStorage.setItem("theme", theme)
-            document.documentElement.classList.add(theme)
-        }
-    }, [])
-
-    return (
-        <ThemeContext.Provider value={{ theme, setTheme }}>
-            {children}
-        </ThemeContext.Provider>
-    )
-
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
 }
 
-export { ThemeContext }
+export { ThemeContext };
